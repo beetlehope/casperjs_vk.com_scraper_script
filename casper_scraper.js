8a246bdd76d85cc9149da7be3fbf5161739acf38
beetlehope <beetlehope@gmail.com>
2018-03-30 20:04:57 +0300

    Fix styling

--- casper_scraper.js
+++ casper_scraper.js
@@ -1,17 +1,17 @@
-var casper = require('casper').create({
+var casper = require("casper").create({
     pageSettings: {
         loadImages: false,
         loadPlugins: false,
-        userAgent: 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
+        userAgent: "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
     }
 });
 
-//Open VK
-casper.start().thenOpen("https://vk.com/", function() {
+// Open VK
+casper.start().thenOpen("https://vk.com", function() {
     console.log("Vk opened");
 });
 
-//Populate the login form with login and password and submit the form
+// Populate the login form with login and password and submit the form
 casper.then(function(){
     console.log("Login using username and password");
     this.evaluate(function(){
@@ -21,32 +21,31 @@ casper.then(function(){
     });
 });
 
-//Wait until you are logged-in and move to a wiki page address
-
-    casper.waitForUrl(/feed/, function(){
-        casper.thenOpen('link_to_the_wiki_page');
-    });
-
-//Click "View" button
-casper.then(function(){
-    this.click('button.flat_button');
+// Wait until you are logged-in and move to a wiki page address
+casper.waitForUrl(/feed/, function(){
+    casper.thenOpen("link_to_the_wiki_page");
 });
 
-//Click "Edit" button   
-casper.waitForSelector('button.flat_button.secondary.wk_subscribe_btn', function(){
-    this.click('button.flat_button.secondary.wk_subscribe_btn');
+// Click "View" button
+casper.then(function(){
+    this.click("button.flat_button");
+});
+
+// Click "Edit" button   
+casper.waitForSelector("button.flat_button.secondary.wk_subscribe_btn", function(){
+    this.click("button.flat_button.secondary.wk_subscribe_btn");
 });    
 
-//Click "Edit in wiki-mode" button 
-casper.waitForSelector('a#wke_b_mode', function(){
-    this.click('a#wke_b_mode');
+// Click "Edit in wiki-mode" button 
+casper.waitForSelector("a#wke_b_mode", function(){
+    this.click("a#wke_b_mode");
 });
 
-//Write the content of the page to a file
+// Write the content of the page to a file
 casper.then(function(){
-    var text = this.fetchText('textarea#wke_textarea');
-    var fs = require('fs');
-    fs.write('wiki.txt', text, 'w');
+    var text = this.fetchText("textarea#wke_textarea");
+    var fs = require("fs");
+    fs.write("wiki.txt", text, "w");
 });
 
 casper.run();
